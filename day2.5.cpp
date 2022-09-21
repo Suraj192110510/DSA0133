283dbc18b97b0d12e66feae85f49675b9807065c
Suraj192110510 <112699069+Suraj192110510@users.noreply.github.com>
2022-09-21 11:43:30 +0530

    Add files via upload

---
+++ day2.5.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int emp_no;
+	string emp_name,dept_name,designation;
+	cout<<"Enter employee number : ";
+	cin>>emp_no;
+	cout<<"Enter employee name : ";
+	cin>>emp_name;
+	cout<<"Enter department name : ";
+	cin>>dept_name;
+	cout<<"Enter employee designation : ";
+	cin>>designation;
+	cout<<"Employee Number is : "<<emp_no<<endl;
+	cout<<"Employee Name is : "<<emp_name<<endl;
+	cout<<"Employee working Department is : "<<dept_name<<endl;
+	cout<<"Employee Designation is : "<<designation;
+}
